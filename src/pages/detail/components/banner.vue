11b3fff2389ae75d54eb7f90e75c9c1b5f80b8cd
fenqiang4952 <495246490@qq.com>
2018-11-22 23:58:36 +0800

    detail banner add

---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_600x330_b40971b4.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">八达岭长城(AAAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary @close="handleGallaryClose" v-show="showGallery" :imgs="imgs"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from '@common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      imgs: [
+        '//img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_600x330_b40971b4.jpg',
+        '//img1.qunarzz.com/sight/p0/1803/f9/f96ac7520e964ad4a3.img.jpg_r_800x800_93b843b1.jpg'
+      ],
+      showGallery: false
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallery = true
+    },
+    handleGallaryClose () {
+      this.showGallery = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      background-image linear-gradient(top, rgba(0,0,0,0), rgba(0, 0, 0, .8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+        color #ffffff
+      .banner-number
+        margin-top .14rem
+        padding 0 .4rem
+        line-height .32rem
+        height .32rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        color #ffffff
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
