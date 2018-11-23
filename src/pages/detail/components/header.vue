5f8c0d22b0fa69e3c9ea5fd98bfe9c745733d746
fenqiang4952 <495246490@qq.com>
2018-11-23 23:59:28 +0800

    detail header

---
+++ src/pages/detail/components/header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont back-abs-icon">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" :style="opacityStyle" v-show="!showAbs">
+      <router-link to="/">
+        <div class="header-left">
+          <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  components: {
+  },
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll (e) {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {
+          opacity
+        }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/varibles.styl'
+  .header-abs
+    position absolute
+    left .2rem
+    top .2rem
+    width .8rem
+    height .8rem
+    line-height .8rem
+    border-radius .4rem
+    text-align center
+    background rgba(0, 0, 0, .8)
+    .back-abs-icon
+      color #ffffff
+      font-size .4rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .32rem
+    .header-left
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      color #ffffff
+      .back-icon
+        font-size .4rem
+</style>
--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="contaner"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
+import DetailHeader from './components/header'
+
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .contaner {
+    height 50rem
+  }
 </style>
