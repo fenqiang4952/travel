39e8f9f5b629f2ddaeb965845e3a1d52460f2dc7
fenqiang4952 <495246490@qq.com>
2018-11-24 01:12:51 +0800

    fixed bugs

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="list">
     <li class="item" v-for="item of letters" :key="item" :ref="item" @click="handleClick(item)"
-      @touchstart="handleTouchStart"
+      @touchstart.prevent="handleTouchStart"
       @touchmove="handleTouchMove"
       @touchend="handleTouchEnd">
       {{item}}
